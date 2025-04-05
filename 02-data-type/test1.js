931b9d7879a158f7e11b833f5d70463adc30ea6a
Nikhil Kumar <56150013+nikhil4u43@users.noreply.github.com>
2025-04-05 16:45:32 +0000

    datatypes

---
+++ 02-data-type/test1.js
@@ -0,0 +1,11 @@
+const value="Qwerty";
+let value1="Nikhil";
+/*
+try not to use var because this is having issues with blocks
+*/
+var value2="Kumar";
+newmethod="Dolly";
+
+newmethod=123456;
+
+console.table([value, value1, value2, newmethod]);
