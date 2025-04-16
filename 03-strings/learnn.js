436c90b40f236907090bf4af17a48e51028e5ca1
Nikhil Kumar <56150013+nikhil4u43@users.noreply.github.com>
2025-04-16 17:50:07 +0000

    string topic

---
+++ 03-strings/learnn.js
@@ -0,0 +1,14 @@
+const nameFirst= "Nikhil"
+const num=50
+
+const str=new String("Qwerty")
+
+console.log(`We want to connect ${nameFirst} with ${num}`)
+console.log(str)
+
+let url ="https://nikhil.com/three%20two"
+console.log(url)
+
+url=url.replace('%20', '-')
+console.log(url)
+console.log(url)
