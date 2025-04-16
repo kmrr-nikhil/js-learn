eb728b81a26968594b44561bde8a85a46e0a4891
Nikhil Kumar <56150013+nikhil4u43@users.noreply.github.com>
2025-04-16 18:56:38 +0000

    learn numbers

---
+++ 04-numberandmath/num.js
@@ -0,0 +1,16 @@
+
+const no = 400;
+console.log(no)
+console.log(typeof no);
+
+
+const noObj=new Number(300)
+console.log(noObj)
+console.log(`*********`)
+console.log(noObj.toString())
+console.log(typeof noObj)
+
+
+const hundred=10000000
+console.log(hundred.toLocaleString())
+console.log(hundred.toLocaleString('en-In'))
