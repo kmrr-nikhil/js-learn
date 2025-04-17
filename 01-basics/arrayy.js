9b1316920ce5b069f00618f31847db6945e38790
Nikhil Kumar <56150013+nikhil4u43@users.noreply.github.com>
2025-04-17 18:31:57 +0000

    array learn

---
+++ 01-basics/arrayy.js
@@ -0,0 +1,15 @@
+const arr=[2,1,7,88,34,2]
+
+console.log(arr[3])
+
+console.log(arr)
+
+
+const qwe= arr.slice(1,3)
+console.log("old array", arr)
+console.log("new array for slice", qwe)
+
+
+const qwer= arr.splice(1,3)
+console.log("old array", arr)
+console.log("new array for slice", qwer)
